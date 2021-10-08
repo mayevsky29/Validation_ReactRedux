e48b6b7358132193f79cc629119730ddbbe99bc7
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-10-08 14:30:21 +0300

    add validation in project

--- AutoPart/frontend/src/components/auth/Login/index.js
+++ AutoPart/frontend/src/components/auth/Login/index.js
@@ -1,29 +1,57 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { Formik, Form } from 'formik';
+import MyTextInput from '../../common/MyTextInput';
+import validationFields from './validation';
+import { useDispatch } from 'react-redux';
+import { LOGIN } from '../../../constants/actionTypes';
 
-// якщо використовується функція то рендерити не потрібно
-// export class LoginPage extends Component {
-    const LoginPage = () => {
-    
-        const[name, setName] = useState()
-        
-        
-       const onClickChangeName =(e) =>{
-            e.preventDefault();
-            setName("Сало");
-        }
-        return (
-            <div>
-                <h1>Вхід на сайт</h1>
-                <input 
-                type="text" 
-                className="form=control"
-                name="name"
-                value={name}
-                />
-                <input type="button" onClick={onClickChangeName} value="Вхід"></input>
-            </div>
-        )
+const LoginPage = () => {
+
+    const initState = {
+        email: '',
+        password: ''
+    }
+    const dispatch = useDispatch();
+
+
+    const onSubmitHandler=(values) => {
+        dispatch({type: LOGIN, payload: values.email});
+        console.log("values submit vf", values)
     }
 
+    return (
+        <div className="row">
+        <div className="offset-md-3 col-md-6">
+        <h1 className="text-center">Вхід на сайт</h1>
+
+            <Formik
+                initialValues={initState}
+                validationSchema = {validationFields()}
+                onSubmit={onSubmitHandler}
+            >
+                <Form>
+                    <MyTextInput
+                        label="Пошта"
+                        id="email"
+                        name="email"
+                        type="text"
+                    />
+
+                     <MyTextInput
+                        label="Пароль"
+                        id="password"
+                        name="password"
+                        type="password"
+                    />
+                     <input type="submit" className="btn btn-success" value="Вхід"></input>
+                </Form>
+            </Formik>
+            
+            </div>
+
+           
+            </div>
+    )
+}
 
 export default LoginPage
--- AutoPart/frontend/src/components/common/MyTextInput.js
+++ AutoPart/frontend/src/components/common/MyTextInput.js
@@ -2,9 +2,9 @@ import { useField } from 'formik';
 import classNames from 'classnames';
 
 const MyTextInput = ({ label, ...props }) => {
-    // useField() повертає [formik.getFieldProps(), formik.getFieldMeta()]
-    // який ми можемо поширювати в <input>. Ми можемо використовувати 
-    // мета поля, щоб показати повідомлення про помилку, якщо поле недійсне і воно було нажате(i.e. visited)
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input>. We can use field meta to show an error
+    // message if the field is invalid and it has been touched (i.e. visited)
     const [field, meta] = useField(props);
     //console.log("fields", field);
     return (
